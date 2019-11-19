8229dc2e0824adb107c32fec1fafe85e77803e8b
Alexey Zheltukhin <azheltukhin@natera.com>
2019-11-19 14:26:51 -0800

    Intial

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  padding: 10px;
-  color: red;
-`;
 
 export interface TestComponentProps {
   text?: string;
 }
 
-export const TestComponent: React.FC<TestComponentProps> = ({ text }) => (
-  <Wrapper>{text ? text : 'Test Component'}</Wrapper>
-);
+export const TestComponent: React.FC<TestComponentProps> = ({ text }) => <div>{text ? text : 'Test Component'}</div>;
