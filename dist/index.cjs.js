73e8b10f2ed29ba5938682c2257e85995284bdae
Alexey Zheltukhin <azheltukhin@natera.com>
2019-11-19 14:43:01 -0800

    Add dist

---
+++ dist/index.cjs.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+var TestComponent = function (_a) {
+    var text = _a.text;
+    return React.createElement("div", null, text ? text : 'Test Component');
+};
+
+exports.TestComponent = TestComponent;
---
+++ dist/index.esm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var TestComponent = function (_a) {
+    var text = _a.text;
+    return React.createElement("div", null, text ? text : 'Test Component');
+};
+
+export { TestComponent };
